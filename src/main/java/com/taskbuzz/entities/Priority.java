db9adbda009d01c249d0d42837e7d835e2a475fb
ULStudent:TANMAY.SRIVASTAVA <23118067@studentmail.ul.ie>
2023-11-26 15:11:20 +0000

    Updated Priority.java after merge conflicts

--- src/main/java/com/taskbuzz/entities/Priority.java
+++ src/main/java/com/taskbuzz/entities/Priority.java
@@ -1,18 +1,10 @@
 package com.taskbuzz.entities;
 
-public enum Priority {
-	LOW(3),
-	HIGH(1),
-	MEDIUM(2);
-	
-	private int priorityLevel;
-	
-	Priority(int priorityLevel) {
-		this.priorityLevel = priorityLevel;
-	}
-	
-	public int getPriorityLevel() {
-		return priorityLevel;
-	}
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
+public enum Priority {
+ LOW,
+ HIGH,
+ MEDIUM
 }
